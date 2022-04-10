ddf35a30397be44ffcc99f4aa29b37cc4a0a9b82
Astan PAtaraya <pataraya951@gmail.com>
2022-04-11 00:52:17 +0300

    html/css/js/ were fixed correctly

--- blog/models.py
+++ blog/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from django.db import models
 from django.urls import reverse
 from django.utils import timezone
-from django.utils.text import slugify
+# from django.utils.text import slugify
 from mptt.models import MPTTModel, TreeForeignKey
 #для обхода бинарныйх деревьевь добавить TreeForeignKey
 
